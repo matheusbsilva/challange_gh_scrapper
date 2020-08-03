7cee1393d2f40d748b3cf040716a608d78fb174e
matheusbsilva <matheusbattista@hotmail.com>
2020-08-03 20:00:12 -0300

    improve request gh class handling exceptions

--- lib/gh_scraper/request_gh.rb
+++ lib/gh_scraper/request_gh.rb
@@ -2,19 +2,14 @@ require 'open-uri'
 
 class RequestGh
 
-  def self.get(uri)
-    new.get(uri)
+  def self.get(profile_url)
+    new.get(profile_url)
   end
 
-  def get(uri)
-    res = URI.open(url(uri))
-    res.read
+  def get(profile_url)
+    res = URI.open(profile_url)
+    { error: false, status: res.status[0], content: res.read }
+  rescue OpenURI::HTTPError => e
+    { error: true, status: e.io.status[0], content: e.io.status[1] }
   end
-
-  private
-
-  def url(uri)
-    "https://github.com#{uri}"
-  end
-
 end
--- spec/lib/gh_scraper/request_gh_spec.rb
+++ spec/lib/gh_scraper/request_gh_spec.rb
@@ -1,16 +1,33 @@
+require 'open-uri'
+require 'tempfile'
+
 require 'spec_helper'
 require 'gh_scraper/request_gh'
-require 'open-uri'
 
 describe RequestGh do
-  let(:uri) { '/foo' }
-  let(:success_response) { Tempfile.new('content') }
+  let(:profile_url) { 'https://github.com/foo' }
+  let(:file) { double }
+  let(:request_success) {
+    { error: false, status: '200', content: 'content' }
+  }
+  let(:request_not_found) {
+    { error: true, status: '404', content: '404 Not Found'}
+  }
 
   describe 'get uri' do
-    it 'returns body response' do
-      # Stub call to URI.open
-      allow(URI).to receive(:open).and_return(success_response)
-      expect(RequestGh.get(uri)).to eq(success_response.read)
+    it 'returns github content on success' do
+      # Stub call to URI.open return
+      allow(file).to receive(:read).and_return('content')
+      allow(file).to receive(:status).and_return(['200', 'OK'])
+      allow(URI).to receive(:open).and_return(file)
+      expect(RequestGh.get(profile_url)).to eq(request_success)
+    end
+
+    it 'returns error status and message on failure' do
+      # Stub call to URI.open return
+      allow(file).to receive(:status).and_return(['404', '404 Not Found'])
+      allow(URI).to receive(:open).and_raise(OpenURI::HTTPError.new('Not Found', file))
+      expect(RequestGh.get(profile_url)).to eq(request_not_found)
     end
   end
 
