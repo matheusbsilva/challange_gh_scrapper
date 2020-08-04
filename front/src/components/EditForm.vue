33fad92c259ac9c9b325c97755f68eda29d57e3a
matheusbsilva <matheusbattista@hotmail.com>
2020-08-04 16:16:24 -0300

    add editform component

---
+++ front/src/components/EditForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col>
+          <b-form-input v-model="username" placeholder="Nome do usuário" class="my-3"></b-form-input>
+          <b-form-input v-model="profileUrl" placeholder="Url do perfil do usuário" class="my-3"></b-form-input>
+          <b-button variant="success" class="my-3">Enviar</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditForm',
+  props: ['id', 'username', 'profileUrl']
+}
+</script>
+
+<style>
+</style>
