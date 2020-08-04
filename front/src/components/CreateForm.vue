ce1ffd2c953c798ba1257553e34aaec5b6576f65
matheusbsilva <matheusbattista@hotmail.com>
2020-08-04 19:47:48 -0300

    validate forms

--- front/src/components/CreateForm.vue
+++ front/src/components/CreateForm.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-  <b-modal 
+      <b-modal 
     id="modal-create" 
     title="Formulário de usuário" 
     @ok="submit"
     >
     <b-container>
+      <p v-if="errors.length">
+        <b>Erro:</b>
+        <ul>
+          <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+      </p>
+
+
       <b-row>
         <b-col>
           <b-form-input v-model="username" placeholder="Nome do usuário" class="my-3"></b-form-input>
@@ -13,9 +21,9 @@
         </b-col>
       </b-row>
     </b-container>
-  </b-modal>
-  <b-modal id="form-create-submitted">
-    <h5>Formulário enviado</h5>
+      </b-modal>
+      <b-modal id="form-create-submitted">
+        <h5>Formulário enviado</h5>
   </b-modal>
   </div>
 </template>
@@ -27,15 +35,35 @@ export default {
     return {
       username: '',
       profileUrl: 'https://github.com/',
+      errors: []
     }
   },
   methods: {
-    async submit() {
-      const body = {'profile': { 'username': this.username, 'profile_url': this.profileUrl }}
-      await this.$store.dispatch('profile/createProfile', body)
+    checkForm() {
+      if(this.username && this.profileUrl.split('https://github.com/').pop()) {
+        return true
+      }
 
-      this.$root.$emit('bv::hide::modal', 'modal-create')
-      this.$root.$emit('bv::show::modal', 'form-create-submitted')
+      this.errors = []
+
+      if(!this.username) {
+        this.errors.push('Insira um nome de usuário')
+      }
+
+      if(!this.profileUrl.split('https://github.com/').pop()) {
+        this.errors.push('Insira uma url de perfil válida ')
+      }
+
+    },
+    async submit(bvModalEvt) {
+      bvModalEvt.preventDefault()
+      if(this.checkForm()) {
+        const body = {'profile': { 'username': this.username, 'profile_url': this.profileUrl }}
+        await this.$store.dispatch('profile/createProfile', body)
+
+        this.$root.$emit('bv::hide::modal', 'modal-create')
+        this.$root.$emit('bv::show::modal', 'form-create-submitted')
+      }
     }
   }
 }
--- front/src/components/EditForm.vue
+++ front/src/components/EditForm.vue
@@ -6,6 +6,14 @@
     @ok="submit"
     >
     <b-container>
+      <p v-if="errors.length">
+        <b>Erro:</b>
+        <ul>
+          <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+      </p>
+
+
       <b-row>
         <b-col>
           <b-form-input v-model="username" placeholder="Nome do usuário" class="my-3"></b-form-input>
@@ -27,7 +35,8 @@ export default {
   props: ['id', 'username'],
   data() {
     return {
-      githubUrl: 'https://github.com/'
+      githubUrl: 'https://github.com/',
+      errors: []
     }
   },
   computed: {
@@ -36,13 +45,33 @@ export default {
     }
   },
   methods: {
-    async submit() {
-      const body = {'profile': { 'username': this.username, 'profile_url': this.profileUrl }}
-      const data = { 'id': this.id, 'body': body }
-      await this.$store.dispatch('profile/updateProfile', data)
+    checkForm() {
+      if(this.username && this.profileUrl.split('https://github.com/').pop()) {
+        return true
+      }
 
-      this.$root.$emit('bv::hide::modal', 'modal-edit')
-      this.$root.$emit('bv::show::modal', 'form-submitted')
+      this.errors = []
+
+      if(!this.username) {
+        this.errors.push('Insira um nome de usuário')
+      }
+
+      if(!this.profileUrl.split('https://github.com/').pop()) {
+        this.errors.push('Insira uma url de perfil válida ')
+      }
+
+    },
+    async submit(bvModalEvt) {
+      bvModalEvt.preventDefault()
+      if(this.checkForm()) {
+
+        const body = {'profile': { 'username': this.username, 'profile_url': this.profileUrl }}
+        const data = { 'id': this.id, 'body': body }
+        await this.$store.dispatch('profile/updateProfile', data)
+
+        this.$root.$emit('bv::hide::modal', 'modal-edit')
+        this.$root.$emit('bv::show::modal', 'form-submitted')
+      }
     }
   }
 }
