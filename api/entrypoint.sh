e7666260196c11a15ed1c81f5934c8c6a256dded
matheusbsilva <matheusbattista@hotmail.com>
2020-08-04 20:55:03 -0300

    add migrate to entrypoint script

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Script to let restart server even if a server.pid exists
 set -e
 
+# let restart server even if a server.pid exists
 rm -f /app/tmp/pids/server.pid
+
+# migrate database
+if rails db:migrate:status &> /dev/null; then
+  rails db:migrate
+else
+  rails db:setup
+fi
 
 exec "$@"
